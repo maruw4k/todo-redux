f27c1380b04ae4d23e2d21033bafd7e28a5908fc
Marek Krzeszowiec <marek.krzeszowiec@alterpage.pl>
2019-09-18 22:01:35 +0200

    Connect redux-devtools-extension
    To easier debug redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
+import { composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(reducer, ["Use Redux"]);
+const store = createStore(reducer, ["Use Redux"], composeWithDevTools());
 
 export default store;
