f6155d8e33400332626c8df04d13f3596542c11e
Marek Krzeszowiec <marek.krzeszowiec@alterpage.pl>
2019-09-19 20:08:09 +0200

    Updates

---
+++ precache-manifest.0d36bfd3a0718c2e89d853dbbbf62ca5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a00c8cf695b0c12feff888b0b80561e",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "38713a5026ce55e3a7e5",
+    "url": "/todo-redux/static/js/2.ddf51986.chunk.js"
+  },
+  {
+    "revision": "0471a865c32ed55a84cf",
+    "url": "/todo-redux/static/js/main.3d752f76.chunk.js"
+  },
+  {
+    "revision": "0986b452d80edbe55ac7",
+    "url": "/todo-redux/static/js/runtime~main.3fc25409.js"
+  }
+]);
