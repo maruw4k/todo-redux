6ce562ef4edc5b2fb86dbddf690d74a92fbe38e8
Marek Krzeszowiec <marek.krzeszowiec@alterpage.pl>
2019-09-19 20:38:47 +0200

    Change background image

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -23,10 +23,10 @@ body {
   margin: 0;
   padding: 0;
     
-  background-color: #330000;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 400'%3E%3Cdefs%3E%3CradialGradient id='a' cx='396' cy='281' r='514' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23D18'/%3E%3Cstop offset='1' stop-color='%23330000'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='400' y1='148' x2='400' y2='333'%3E%3Cstop offset='0' stop-color='%23FA3' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23FA3' stop-opacity='0.5'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='400'/%3E%3Cg fill-opacity='0.4'%3E%3Ccircle fill='url(%23b)' cx='267.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='532.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='400' cy='30' r='300'/%3E%3C/g%3E%3C/svg%3E");
-  background-attachment: fixed;
-  background-size: cover;
+background-color: #000000;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%230c1122' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23182244' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23253366' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23314488' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%233d55aa' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
 
   display: flex;
   justify-content: center;
